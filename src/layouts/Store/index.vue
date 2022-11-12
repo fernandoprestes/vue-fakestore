2d2924bd4355707cbe2126529aa6df8363d76a76
fernandoprestes <fernandoprestesc@gmail.com>
2022-11-12 16:04:19 -0300

    refactor(layouts): refactor sidebar home store

--- src/layouts/Store/index.vue
+++ src/layouts/Store/index.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
   import TheHeader from './TheHeader.vue';
-  import TheSidebar from './TheSidebar.vue';
 </script>
 <template>
   <TheHeader />
-  <main class="flex h-[calc(100vh-74px)] flex-col md:grid md:grid-cols-[278px,_1fr]">
-    <TheSidebar />
-    <RouterView />
-  </main>
+  <RouterView />
 </template>
